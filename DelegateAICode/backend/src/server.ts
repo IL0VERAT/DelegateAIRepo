b5e0710825ee4a7cf2572ff3f1a5702c171f0de4
IL0VERAT <26mlinnboggs@derryfield.org>
2025-08-02 13:51:00 -0400

    Fixing issue with Prisma migrations

--- DelegateAICode/backend/src/server.ts
+++ DelegateAICode/backend/src/server.ts
@@ -9,7 +9,7 @@ Sentry.init({
   integrations: [
     nodeProfilingIntegration(),
   ],
-  debug: true,  //REMOVE
+  debug: false,  //REMOVE
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set sampling rate for profiling - this is evaluated only once per SDK.init call
